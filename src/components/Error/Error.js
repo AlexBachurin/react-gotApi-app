47184adcc42f95c9d477ff530741ddcf54680a12
Alex <alexbachurin91@gmail.com>
2021-09-22 13:27:06 +0300

    error image import

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './Error.css'
+import img from './sad_face.jpg'
 export default class Error extends Component {
     render() {
         return (
             <div className="random-block rounded">
-                <img className="error-image" src={process.env.PUBLIC_URL + '/img/sad_face.jpg'} alt="sad-face" />
+                <img className="error-image" src={img} alt="sad-face" />
                 <h3 className="error-message">Oops, Something went wrong!</h3>
             </div>
         )
