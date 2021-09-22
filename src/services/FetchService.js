ab9e2dcd45f727b14a6f8ca504caf4af6eac1770
Alex <alexbachurin91@gmail.com>
2021-09-22 11:44:11 +0300

    display random character on page load

--- src/services/FetchService.js
+++ src/services/FetchService.js
@@ -9,40 +9,45 @@ export default class FetchService extends Component {
     }
     //ultimate function to get data from provided url
     async getData(url) {
-        const res = await fetch(`${this._baseURL}${url}`);
-        const data = await res.json();
-        return data;
+        try {
+            const res = await fetch(`${this._baseURL}${url}`);
+            //also check for proper url
+            if (!res.ok) {
+                throw new Error(`Error ${res.error} status: ${res.status}`)
+            }
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
 
     }
     //get all characters from 1 page
     getAllCharacters() {
-        this.getData('/characters?page=5&pageSize=10')
-            .then(data => console.log(data));
+        return this.getData('/characters?page=5&pageSize=10')
     }
     //get 1 char by id
     getSingleCharacter(id) {
-        this.getData(`/characters/${id}`)
-            .then(data => console.log(data));
+        return this.getData(`/characters/${id}`);
     }
     //get Books
     getAllBooks() {
-        this.getData('/books')
-            .then(data => console.log(data));
+        return this.getData('/books')
     }
     //get book by id
     getSingleBook(id) {
-        this.getData(`/books/${id}`)
-            .then(data => console.log(data));
+        return this.getData(`/books/${id}`);
+
     }
     //get houses
     getAllHouses() {
-        this.getData('/houses')
-            .then(data => console.log(data));
+        this.getData('/houses');
+
     }
     //get specific house
     getSingleHouse(id) {
-        this.getData(`/houses/${id}`)
-            .then(data => console.log(data));
+        this.getData(`/houses/${id}`);
+
     }
 }
 
